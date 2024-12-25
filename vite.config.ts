b12dc1c4e70599f54728ac17a7654e6cf68f3fc7
Adnan Habib <adnanhabib2003@gmail.com>
2024-12-25 04:50:30 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-        https: {
-            key: fs.readFileSync('certs/localhost-key.pem'),
-            cert: fs.readFileSync('certs/localhost.pem'),
-        },
-        port: 4000, // Custom port for development
-    },
     plugins: [react()],
     optimizeDeps: {
         exclude: ['lucide-react'],
